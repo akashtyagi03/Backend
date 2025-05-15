52b20f29748769d5810ae0ceb15d7438a698fd39
Aakash tyagi <akashtyagi780251@gmail.com>
2025-05-15 11:36:05 +0530

    learn about zod library & authentication

--- index.js
+++ index.js
@@ -1,45 +1,20 @@
 const express = require("express")
+const {z} = require("zod")
 const app = express()
 
+const schema = z.string().email();
+app.use(express.json());
 
-function usermiddleware(req, res, next){
-    const username = req.headers.username;
-    const password = req.headers.password;
-    if (username != "akash" || password!="pass"){
-        res.status(411).json({
-            msg:"you given us wrong input"
+
+app.post("/health-checkup",(req, res)=>{
+    const kidney = req.body.kidney;
+    const response = schema.safeParse(kidney)
+    if (!response.success){
+        res.status(400).json({
+            msg:"wrong input"
         })
-        return;
-    }else{
-        next();
     }
-}
-function kidneymiddleware(req, res, next){
-    const kidneyid = req.query.kidneyid;
-    const kidneylength = kidneyid.length;
-
-    res.send("you have " + kidneylength + " kidney");
-    // if (kidneyid!=1 && kidneyid!=2){
-    //     res.status(400).json({
-    //         msg: "you give wrong id"
-    //     })
-    //     return;
-    // }else{
-    //     next();
-    // }
-}
-app.use(usermiddleware)
-
-
-app.get("/health-checkup", kidneymiddleware, (req, res)=>{
-    
-    res.json({ message: "your kidney is fine!" })
-})
-
-app.use(function(err, req, res, next){
-    res.json({
-        msg: "here some issue with our server"
-    })
-})
+    res.send({ response})
+});
 
 app.listen(3000)
